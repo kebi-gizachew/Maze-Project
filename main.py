04235909ad017a34a0233f544c32c8b2de717071
Kebron Gizachew <kebisel21@gmail.com>
2026-05-07 13:54:06 +0300

    putting the maze wall structure and drawing a line

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from OpenGL.GLU import *
 
 WIDTH = 800
 HEIGHT = 800
+ROWS = 20
+COLS = 20
+CELL_SIZE = 2 / ROWS
+northWall = [[1 for _ in range(COLS)] for _ in range(ROWS)]
+eastWall = [[1 for _ in range(COLS)] for _ in range(ROWS)]
 
+visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
 pygame.init()
 
 pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL)
@@ -13,6 +19,47 @@ pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL)
 glClearColor(0, 0, 0, 1)
 
 running = True
+def draw_maze():
+
+    for row in range(ROWS):
+
+        for col in range(COLS):
+
+            x = -1 + col * CELL_SIZE
+            y = 1 - row * CELL_SIZE
+
+            # NORTH WALL
+            if northWall[row][col]:
+
+                draw_line(
+                    x,
+                    y,
+                    x + CELL_SIZE,
+                    y
+                )
+
+            # EAST WALL
+            if eastWall[row][col]:
+
+                draw_line(
+                    x + CELL_SIZE,
+                    y,
+                    x + CELL_SIZE,
+                    y - CELL_SIZE
+                )
+def draw_grid():
+
+    for row in range(ROWS + 1):
+
+        y = 1 - row * CELL_SIZE
+
+        draw_line(-1, y, 1, y)
+
+    for col in range(COLS + 1):
+
+        x = -1 + col * CELL_SIZE
+
+        draw_line(x, -1, x, 1)
 def draw_line(x1, y1, x2, y2):
 
     glBegin(GL_LINES)
@@ -30,7 +77,11 @@ while running:
             running = False
 
     glClear(GL_COLOR_BUFFER_BIT)
-    draw_line(-0.5, 0, 0.5, 0)
+    draw_grid()
     pygame.display.flip()
+# LEFT BORDER
+draw_line(-1, -1, -1, 1)
+# BOTTOM BORDER
+draw_line(-1, -1, 1, -1)
 
 pygame.quit()
